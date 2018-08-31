e7203210dfdf24a813dbfcf294f19225af667fd2
Sakthivel Eka <s.vel743@gmail.com>
2018-08-31 15:35:25 +0530

    Added create, retrieve,update,partial_update and destroy methods to our ViewSet.

--- src/profiles_proj/profiles_api/views.py
+++ src/profiles_proj/profiles_api/views.py
@@ -39,6 +39,9 @@ class HelloAPIView(APIView):
         return Response({'method':'delete'})
 
 class HelloViewSet(viewsets.ViewSet):
+
+    serializer_class=serializers.HelloSerializer
+
     def list(self,request):
         a_viewset=[
             "Uses actions(List,Create,Retreive,Update,Partial_update,Destroy)",
@@ -46,3 +49,24 @@ class HelloViewSet(viewsets.ViewSet):
             "More functionalities with less code.",
         ]
         return Response({'message':'Hello!!!','a_viewset':a_viewset})
+
+    def create(self,request):
+        serializer=serializers.HelloSerializer(data=request.data)
+        if serializer.is_valid():
+            name=serializer.data.get('name')
+            message="Hello {0}!".format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self,request,pk=None):
+        return Response({'http_method':'GET'})
+
+    def update(self,request,pk=None):
+        return Response({'http_method':"PUT"})
+
+    def partial_update(self,request,pk=None):
+        return Response({'http_method':'PATCH'})
+
+    def destroy(self,request,pk=None):
+        return Response({'http_method':'DELET'})
