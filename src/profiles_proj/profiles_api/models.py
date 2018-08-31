1ce55dd6a87cd46b8e951538b3be61a8e859c8d2
Sakthivel Eka <s.vel743@gmail.com>
2018-08-31 11:08:28 +0530

    Enabled to Django Admin for User Profiles Model.

--- src/profiles_proj/profiles_api/models.py
+++ src/profiles_proj/profiles_api/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import BaseUserManager
 
 
 # Create your models here.
-class UserProfileManager():
+class UserProfileManager(BaseUserManager):
     def create_user(self,email,name,password=None):
         if not email:
             raise ValueError('User shd have email id.!')
