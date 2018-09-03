cf183daf3a15a167bbe9b5070d01e0ffc115c004
Sakthivel Eka <s.vel743@gmail.com>
2018-09-03 13:22:29 +0530

    Added search functions to user profile APIs.

--- src/profiles_proj/profiles_api/views.py
+++ src/profiles_proj/profiles_api/views.py
@@ -12,6 +12,8 @@ from . import models
 from . import permissions
 
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+
 
 class HelloAPIView(APIView):
     serializer_class=serializers.HelloSerializer
@@ -80,3 +82,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset= models.UserProfile.objects.all()
     authentication_classes=(TokenAuthentication,)
     permission_classes=(permissions.UpdateOwnProfile,)
+    filter_backends=(filters.SearchFilter,)
+    search_fields=('name','email')
