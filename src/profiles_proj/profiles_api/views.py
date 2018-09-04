edc659319005063cc529630ff38f01ff7ee63d6e
Sakthivel Eka <s.vel743@gmail.com>
2018-09-04 17:39:45 +0530

    Staging all changes

--- src/profiles_proj/profiles_api/views.py
+++ src/profiles_proj/profiles_api/views.py
@@ -17,6 +17,8 @@ from rest_framework import filters
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 class HelloAPIView(APIView):
     serializer_class=serializers.HelloSerializer
     def get(self,request,format=None):
@@ -85,9 +87,18 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     authentication_classes=(TokenAuthentication,)
     permission_classes=(permissions.UpdateOwnProfile,)
     filter_backends=(filters.SearchFilter,)
-    search_fields=('name','email')
+    search_fields=('name','email',)
 
 class LoginViewSet(viewsets.ViewSet):
     serializer_class=AuthTokenSerializer
     def create(self,request):
         return ObtainAuthToken().post(request)
+
+class ProfileFeedItemViewSet(viewsets.ModelViewSet):
+    authentication_classes=(TokenAuthentication,)
+    serializer_class=serializers.ProfileFeedItemSerializer
+    queryset=models.ProfileFeedItem.objects.all()
+    permission_classes=(permissions.PostOwnStatus, IsAuthenticatedOrReadOnly, )
+
+    def perform_create(self,serializer):
+        serializer.save(user_profile=self.request.user)
